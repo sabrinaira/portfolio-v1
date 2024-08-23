d9496b52599dcb49d9d2342f41a6dfb7098dba62
Sabrina Ira <85188006+sabrinaira@users.noreply.github.com>
2024-08-23 01:47:27 -0400

    button symbol update, project page polish

--- portfolio_toggle-button.js
+++ portfolio_toggle-button.js
@@ -4,6 +4,7 @@ const btn = document.getElementById('toggle-button');
 const theme = document.getElementById('theme-link');
 const image = document.getElementById('darkModeImg');
 const fcIcon = document.getElementById('fc1');
+const symbol = document.getElementById('button-symbol');
 
 // Listen for a click on the button
 btn.addEventListener('click', function () {
@@ -11,12 +12,15 @@ btn.addEventListener('click', function () {
   if (theme.getAttribute('href') == 'portfolio-styles.css') {
     // ... then switch it to "dark-theme.css"
     theme.href = 'portfolio-styles_dark.css';
+    symbol.src = 'assets/icons8-sun-32.png';
     fcIcon.src = 'assets/future-code-1-logo-darkblue_64.png';
     image.src = 'assets/sabrinaira-profile-pic-dark.jpg';
+
     // Otherwise...
   } else {
     // ... switch it to "light-theme.css"
     theme.href = 'portfolio-styles.css';
+    symbol.src = 'assets/icons8-dark-mode-32.png';
     fcIcon.src = 'assets/future-code-1-logo_64.png';
     image.src = 'assets/sabrinaira-profile-pic-light.jpg';
   }
